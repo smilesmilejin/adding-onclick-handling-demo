4ac89d54f0e70abbcb5dab2cb34a3f9cb7652df2
Xinshuang Jin <jinxinshuang@gmail.com>
2025-05-21 15:27:44 -0400

    event object

--- src/index.js
+++ src/index.js
@@ -1,13 +1,42 @@
-const addCrab = () => {
+// // make a function to run when it occurs
+// const addCrab = () => {
+//   // Create a span element that will contain our new crab
+//   const newCrab = document.createElement('span');
+//   const crabContainer = document.querySelector('#crabContainer');
+//   newCrab.textContent = '🦀';
+//   crabContainer.appendChild(newCrab);
+// };
+
+// // Select the HTML elemtn the event will occur on
+// const registerEventHandlers = () => {
+//   const crabButton = document.querySelector('#addCrabButton');
+//   crabButton.addEventListener('click', addCrab);
+// };
+
+// // Register that fucntion as 'even listener'
+// document.addEventListener('DOMContentLoaded', registerEventHandlers);
+
+
+// The Event Object
+// When we introduced the addEventListener function, 
+// we mentioned that the event-handling callback function we passed in 
+// could accept an event argument.
+
+const addCrab = (event) => {
+  console.log('in addCrab:', event);
+  // ... other addCrab functionality
+    // Create a span element that will contain our new crab
   const newCrab = document.createElement('span');
   const crabContainer = document.querySelector('#crabContainer');
   newCrab.textContent = '🦀';
   crabContainer.appendChild(newCrab);
 };
 
-const registerEventHandlers = () => {
+const registerEventHandlers = (event) => {
+  console.log('in registerEventHandlers:', event);
   const crabButton = document.querySelector('#addCrabButton');
   crabButton.addEventListener('click', addCrab);
 };
 
+// Register that fucntion as 'even listener'
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
